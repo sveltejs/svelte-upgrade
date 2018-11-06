98aa4888e4b7439e9493172e3645f7260e7096f1
Rich Harris <richard.a.harris@gmail.com>
2018-11-06 09:08:37 -0500

    handle multiple inputs

--- src/cli.js
+++ src/cli.js
@@ -19,6 +19,37 @@ function mkdirp(dir) {
 	}
 }
 
+const valid_extensions = new Set([
+	'.html',
+	'.htmlx',
+	'.svelte'
+]);
+
+function get_tasks(items, in_dir, out_dir, arr = []) {
+	for (const item of items) {
+		const stats = fs.statSync(item);
+
+		if (stats.isDirectory()) {
+			get_tasks(
+				fs.readdirSync(item).map(file => path.resolve(item, file)),
+				path.join(in_dir, file),
+				path.join(out_dir, file),
+				arr
+			);
+		} else {
+			if (valid_extensions.has(path.extname(item))) {
+				arr.push({
+					input: item,
+					output: item.replace(in_dir, out_dir),
+					source: fs.readFileSync(item, 'utf-8')
+				});
+			}
+		}
+	}
+
+	return arr;
+}
+
 [2, 3].forEach(v => {
 	prog.command(`v${v} <input>`)
 		.describe(`upgrade <input> file/directory to v${v} syntax`)
@@ -27,30 +58,36 @@ function mkdirp(dir) {
 		.example(`v${v} MyComponent.html`)
 		.example(`v${v} MyComponent.html -o My-Component.v${v}.html`)
 		.example(`v${v} src`)
-		.action(async (input, opts) => {
-			try {
-				const stats = fs.statSync(input);
+		.action(async function(_, opts) {
+			const tasks = get_tasks(
+				[_].concat(opts._).map(file => path.resolve(file)),
+				path.resolve('.'),
+				path.resolve(opts.output || '.')
+			);
 
+			if (opts.output && path.extname(opts.output)) {
+				// --output somefile.html
+				if (tasks.length > 1) {
+					console.error(c.bold.red(`--output must be a directory if more than one input is provided`));
+				} else {
+					// special case — file to file conversion
+					tasks[0].output = path.resolve(opts.output);
+				}
+			}
+
+			try {
 				const upgrade = v === 2
 					? await import('./v2/index.js')
 					: await import('./v3/index.js');
 
-				let output = opts.output || input;
-
-				if (stats.isDirectory()) {
-					const files = glob.sync('**/*.+(html|htmlx|svelte)', { cwd: input });
-					input = files.map(file => path.join(input, file));
-					output = files.map(file => path.join(output, file));
-				} else {
-					input = [input];
-					output = [output];
-				}
-
 				if (!opts.force) {
-					const existing = output.filter(file => fs.existsSync(file));
+					const existing = tasks
+						.filter(task => fs.existsSync(task.output))
+						.map(task => path.relative(process.cwd(), task.output));
+
 					if (existing.length > 0) {
 						console.error(c.cyan(`This will overwrite the following files:`));
-						console.error(c.gray(existing.join('\n')))
+						console.error(c.gray(existing.join('\n')));
 
 						const response = await prompts({
 							type: 'confirm',
@@ -66,31 +103,45 @@ function mkdirp(dir) {
 					}
 				}
 
-				input.forEach((src, i) => {
-					const dest = output[i];
+				let unchanged_count = 0;
 
+				tasks.forEach(({ input, output, source }) => {
 					try {
-						const upgraded = upgrade.upgradeTemplate(fs.readFileSync(src, 'utf-8'));
+						const result = upgrade.upgradeTemplate(source);
 
-						mkdirp(path.dirname(dest));
-						fs.writeFileSync(dest, upgraded);
-					} catch (err) {
-						console.error(c.bold.red(`Error transforming ${src}:`));
-						console.error(c.red(err.message));
+						if (result.trim() === source.trim()) {
+							unchanged_count += 1;
+						}
 
-						if (err.frame) {
-							console.error(err.frame);
+						// we still write, even if unchanged, since the output dir
+						// could be different to the input dir
+						mkdirp(path.dirname(output));
+						fs.writeFileSync(output, result);
+					} catch (error) {
+						console.error(c.bold.red(`Error transforming ${input}:`));
+						console.error(c.red(error.message));
+
+						if (error.frame) {
+							console.error(error.frame);
 						}
 					}
 				});
 
-				console.error(c.cyan(`Wrote ${output.length} ${output.length === 1 ? 'file' : 'files'}`))
+				let message = `Wrote ${count(tasks.length)}`;
+
+				if (unchanged_count > 0) {
+					message += `. ${count(unchanged_count)} components required no changes`;
+				}
+
+				console.error(c.cyan(message));
 			} catch (err) {
 				console.error(c.red(err.message));
 			}
 		});
 });
 
-
+function count(num) {
+	return num === 1 ? `1 file` : `${num} files`;
+}
 
 prog.parse(process.argv);
