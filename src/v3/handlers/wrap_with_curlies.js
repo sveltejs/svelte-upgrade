d50479a591e36d4669b0edf5d73d385b26f2c23b
Rich Harris <richard.a.harris@gmail.com>
2019-01-29 14:26:36 -0500

    disregard shorthand

--- src/v3/handlers/wrap_with_curlies.js
+++ src/v3/handlers/wrap_with_curlies.js
@@ -1,4 +1,8 @@
 export default function wrap_with_curlies(node, info) {
+	// disregard shorthand
+	const match = /^(\w+):(\w+)/.exec(info.code.original.slice(node.start, node.end));
+	if (node.start + match[0].length === node.end) return;
+
 	const expression = node.expression || node.value;
 
 	if (!expression) return;
